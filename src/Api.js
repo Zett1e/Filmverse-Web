7d1ffa457b2cf06554c7129bf73190bf4a952d94
Thaw Zin <thawzzin.16@gmail.com>
2023-08-12 00:19:49 +0630

    fix:remove gh-pages

--- src/Api.js
+++ src/Api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import api_key from "./ApiKey";
 
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: api_key,
+    api_key: process.env.REACT_APP_API_KEY,
   },
 });
